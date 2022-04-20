6c62a4397c18574af5db985109042fb8c964fec9
HomieKim <ghals0708@gmail.com>
2022-04-20 20:27:27 +0900

    feat: 유저정보테이블 ui 구현, dummy data 생성

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,8 +1,68 @@
+import React, { useCallback, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableContainer,
+  TableFooter,
+  TablePagination,
+  Paper,
+  TableRow,
+} from '@mui/material';
+import { getdummyUser } from './dummy';
+
+const users = getdummyUser();
+
 export default function UserPage() {
+  const [page, setPage] = useState(0);
+
+  const handleChangePage = useCallback((event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+      setPage(newPage);
+    },[page]);
+    
   return (
     <div>
       <h1>User Page</h1>
-      hello
+      <div className={`px-6 pt-6`}>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align='center'>Id</TableCell>
+                <TableCell align='center'>Email</TableCell>
+                <TableCell align='center'>Nickname</TableCell>
+                <TableCell align='center'>date joined</TableCell>
+                <TableCell align='center'>last login</TableCell>
+                <TableCell align='center'>status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.slice(page * 10, (page + 1) * 10).map(v => (
+                <TableRow key={v.id}>
+                  <TableCell align='center'>{v.id}</TableCell>
+                  <TableCell align='center'>{v.email}</TableCell>
+                  <TableCell align='center'>{v.nickname}</TableCell>
+                  <TableCell align='center'>{v.dateJoin}</TableCell>
+                  <TableCell align='center'>{v.lastLogin}</TableCell>
+                  <TableCell align='center'>버튼</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  count={users.length}
+                  page={page}
+                  rowsPerPage={10}
+                  onPageChange={handleChangePage}
+                  rowsPerPageOptions={[]}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 }
--- src/types/common.ts
+++ src/types/common.ts
@@ -5,3 +5,20 @@ export type SidebarType = {
   path: string;
   icon: ReactNode;
 };
+
+export type User = {
+  id: number;
+  email: string;
+  nickname: string;
+  dateJoin: string;
+  lastLogin: string;
+  vedio: Vedio[];
+};
+
+export type Vedio = {
+  id: number,
+  type: string,
+  description: string,
+  thumbnail: string,
+  url: string,
+};
