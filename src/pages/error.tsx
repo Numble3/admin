e02a6d8308022b739bc03638e0ff9c901257663f
yena <center7332@naver.com>
2022-04-21 22:07:34 +0900

    feat: error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,3 +1,26 @@
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import { useNavigate } from 'react-router-dom';
+
 export default function ErrorPage() {
-  return <div>error page</div>;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className='grid h-full w-full place-items-center bg-gray-200'>
+      <div className='flex flex-col items-center'>
+        <CancelOutlinedIcon sx={{ width: '20ch', height: '20ch', color: 'white' }} />
+        <h1 className='font-semibold text-gray-500'>잘못된 접근입니다.</h1>
+
+        <button
+          onClick={handleBack}
+          className='mt-10 cursor-pointer rounded-lg bg-white py-3 px-20 font-medium text-gray-500 shadow-lg shadow-gray-300'
+        >
+          돌아가기
+        </button>
+      </div>
+    </div>
+  );
 }
