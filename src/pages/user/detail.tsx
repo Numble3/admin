7c995556996095264ad1dbdaf3b97723ab04d0d6
HomieKim <ghals0708@gmail.com>
2022-04-22 02:40:27 +0900

    feat: user detail page

--- src/pages/user/detail.tsx
+++ src/pages/user/detail.tsx
@@ -1,11 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { User } from 'src/types/common';
+import Profile from './profile';
+import CustomTable from './table';
+
+const headers: string[] = ['id', 'type', 'title', 'description', 'thumbnail', 'video url', ' '];
+
+interface RouteState {
+  state: {
+    user: User;
+  };
+}
 
 export default function UserDetailPage() {
-  const { id } = useParams();
+  const location = useLocation() as RouteState;
+  const user = location.state.user;
+
   return (
-    <div>
-      <h1>User Detail</h1>
-      id : {id}
+    <div className={`px-6 pt-6 `}>
+      <h1 className={`mb-2 font-bold`}>User {'>'} DashBoard</h1>
+      <h3>Profile</h3> <hr />
+      <Profile user={user} />
+      <h3>Upload Video</h3> <hr className='mb-3' />
+      <CustomTable tableType='video' headers={headers} videoList={user.video} />
     </div>
   );
 }
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,67 +1,17 @@
 import React, { useCallback, useState } from 'react';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableContainer,
-  TableFooter,
-  TablePagination,
-  Paper,
-  TableRow,
-} from '@mui/material';
 import { getdummyUser } from './dummy';
+import { useNavigate } from 'react-router-dom';
+import CustomTable from './table';
 
 const users = getdummyUser();
+const headers: string[] = ['user id', 'Email', 'nickname', 'dated joined', 'last login', 'status'];
 
 export default function UserPage() {
-  const [page, setPage] = useState(0);
-
-  const handleChangePage = useCallback((event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-      setPage(newPage);
-    },[page]);
-    
   return (
     <div>
-      <h1>User Page</h1>
-      <div className={`px-6 pt-6`}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align='center'>Id</TableCell>
-                <TableCell align='center'>Email</TableCell>
-                <TableCell align='center'>Nickname</TableCell>
-                <TableCell align='center'>date joined</TableCell>
-                <TableCell align='center'>last login</TableCell>
-                <TableCell align='center'>status</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {users.slice(page * 10, (page + 1) * 10).map(v => (
-                <TableRow key={v.id}>
-                  <TableCell align='center'>{v.id}</TableCell>
-                  <TableCell align='center'>{v.email}</TableCell>
-                  <TableCell align='center'>{v.nickname}</TableCell>
-                  <TableCell align='center'>{v.dateJoin}</TableCell>
-                  <TableCell align='center'>{v.lastLogin}</TableCell>
-                  <TableCell align='center'>버튼</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  count={users.length}
-                  page={page}
-                  rowsPerPage={10}
-                  onPageChange={handleChangePage}
-                  rowsPerPageOptions={[]}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
+      <h1 className={`px-6 pt-6 font-bold`}>유저</h1>
+      <div>
+        <CustomTable tableType='user' headers={headers} userList={users} />
       </div>
     </div>
   );
